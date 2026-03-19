7552cead35718a24dd46f124a4c643ab898bc2eb
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2026-03-19 17:55:02 +0900

    initial commit

--- src/FSMTorquePDTaskTester.cpp
+++ src/FSMTorquePDTaskTester.cpp
@@ -26,24 +26,25 @@ FSMTorquePDTaskTester::FSMTorquePDTaskTester(mc_rbdyn::RobotModulePtr rm, double
     mc_rtc::log::info(" - {}", frame);
   }
 
-  std::string tool_frame = config("frame_name", (std::string) "tool_frame");
-  std::string relative_frame = config("relative_frame_name", (std::string) "world");
+  std::string left_hand_frame = config("left_hand_frame_name", (std::string) "left_hand_link");
+  std::string right_hand_frame = config("right_hand_frame_name", (std::string) "right_hand_link");
+  // std::string relative_frame = config("relative_frame_name", (std::string) "world");
 
+  LeftHandTask = std::make_shared<mc_tasks::TorquePDRelativeCartesianTask>(
+      solver(), left_hand_frame, robots(), robot().robotIndex());
+  LeftHandTask->setCompensateGravity(true);
+  solver().addTask(LeftHandTask);
 
-  torquePDCartesianTask = std::make_shared<mc_tasks::TorquePDCartesianTask>(
-      solver(), tool_frame, robot().robotIndex());
-  torquePDCartesianTask->setCompensateGravity(true);
-  
-  solver().addTask(torquePDCartesianTask);
-
-  // torquePDRelativeCartesianTask = std::make_shared<mc_tasks::TorquePDRelativeCartesianTask>(
-  //     solver(), tool_frame, relative_frame, robots(), robot().robotIndex());
-  // torquePDRelativeCartesianTask->setCompensateGravity(true);
-  // solver().addTask(torquePDRelativeCartesianTask);
+  RightHandTask = std::make_shared<mc_tasks::TorquePDRelativeCartesianTask>(
+      solver(), right_hand_frame, robots(), robot().robotIndex());
+  RightHandTask->setCompensateGravity(true);
+  solver().addTask(RightHandTask);
 
   datastore().make<std::string>("ControlMode", "Torque");
-  datastore().make<std::string>("TorqueMode", "Custom");
-  datastore().make_call("getPostureTask", [this]() -> mc_tasks::PostureTaskPtr { return postureTask; });
+  if(!datastore().has("anchorFrameFunction"))
+  {
+    datastore().make_call("anchorFrameFunction", [this](const mc_rbdyn::Robot & robot) {return createContactAnchor(robot);});
+  }
 
   mc_rtc::log::success("FSMTorquePDTaskTester init done ");
 }
@@ -57,4 +58,33 @@ bool FSMTorquePDTaskTester::run()
 void FSMTorquePDTaskTester::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::fsm::Controller::reset(reset_data);
+}
+
+std::pair<sva::PTransformd, Eigen::Vector3d> FSMTorquePDTaskTester::createContactAnchor(const mc_rbdyn::Robot & anchorRobot)
+{
+  sva::PTransformd X_foot_r = anchorRobot.bodyPosW("right_ankle_link");
+  sva::PTransformd X_foot_l = anchorRobot.bodyPosW("left_ankle_link");
+
+  sva::MotionVecd v_foot_r = anchorRobot.bodyVelW("right_ankle_link");
+  sva::MotionVecd v_foot_l = anchorRobot.bodyVelW("left_ankle_link");
+
+  auto extTorqueSensor = robot().device<mc_rbdyn::VirtualTorqueSensor>("ExtTorquesVirtSensor");
+  // double tau_ext_knee_r = extTorqueSensor.torques()[robot().jointIndexByName("right_knee_joint")];
+  // double tau_ext_knee_l = extTorqueSensor.torques()[robot().jointIndexByName("left_knee_joint")];
+  double tau_ext_knee_r =  abs(extTorqueSensor.torques()[8+6]);
+  double tau_ext_knee_l =  abs(extTorqueSensor.torques()[3+6]);
+  if(tau_ext_knee_r + tau_ext_knee_l < 1e-6)
+  {
+    tau_ext_knee_l = 1;
+    tau_ext_knee_r = 1;
+  }
+  double leftFootRatio = tau_ext_knee_l/(tau_ext_knee_r+tau_ext_knee_l);
+                              
+  Eigen::VectorXd w_r = X_foot_r.translation();//* tau_ext_knee_r/(tau_ext_knee_r+tau_ext_knee_l);
+  Eigen::VectorXd w_l = X_foot_l.translation(); //* tau_ext_knee_l/(tau_ext_knee_r+tau_ext_knee_l);
+  Eigen::VectorXd contact_anchor = (w_r * (1 - leftFootRatio) + w_l * leftFootRatio)  ;
+  Eigen::VectorXd anchor_vel = (v_foot_r.linear() * (1 - leftFootRatio) + v_foot_l.linear() * leftFootRatio);
+  sva::PTransformd contact_anchor_tf(Eigen::Matrix3d::Identity(), contact_anchor); 
+
+  return {contact_anchor_tf, anchor_vel};
 }
--- src/FSMTorquePDTaskTester.h
+++ src/FSMTorquePDTaskTester.h
@@ -2,7 +2,6 @@
 
 #include <mc_control/fsm/Controller.h>
 #include <mc_tasks/TorquePDJointTask.h>
-#include <mc_tasks/TorquePDCartesianTask.h>
 #include <mc_tasks/TorquePDRelativeCartesianTask.h>
 #include "api.h"
 
@@ -15,8 +14,10 @@ struct FSMTorquePDTaskTester_DLLAPI FSMTorquePDTaskTester : public mc_control::f
   void reset(const mc_control::ControllerResetData & reset_data) override;
 
   std::shared_ptr<mc_tasks::TorquePDJointTask> torquePDTask;
-  std::shared_ptr<mc_tasks::TorquePDCartesianTask> torquePDCartesianTask;
-  std::shared_ptr<mc_tasks::TorquePDRelativeCartesianTask> torquePDRelativeCartesianTask;
+  std::shared_ptr<mc_tasks::TorquePDRelativeCartesianTask> RightHandTask;
+  std::shared_ptr<mc_tasks::TorquePDRelativeCartesianTask> LeftHandTask;
+
+  std::pair<sva::PTransformd, Eigen::Vector3d>  createContactAnchor(const mc_rbdyn::Robot & anchorRobot);
 private:
   mc_rtc::Configuration config_;
 };
